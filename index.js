60f45e466068d43edabea2e906cb08533dcff9e2
1of1Melo <trninicmatej08@gmail.com>
2025-05-02 11:40:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ index.js
 const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
-  host: 'mc.server-ip.com', // ZAMENI sa IP adresom tvog servera
-  port: 25565,              // standardni Minecraft port
+  host: 'milossmrad.aternos.me', // ZAMENI sa IP adresom tvog servera
+  port: 21233,              // standardni Minecraft port
   username: 'BotIme',       // može i email ako je premium nalog
-  version: '1.20.1'         // verzija koju bot koristi (1.21.4 još nije podržana!)
+  version: '1.21.4'         // verzija koju bot koristi (1.21.4 još nije podržana!)
 });
 
 bot.on('chat', (username, message) => {
